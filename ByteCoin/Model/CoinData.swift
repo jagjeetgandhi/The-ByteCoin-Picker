aba4a4e720e225c59c9f98d3809942b67955f770
sejalllkhanna <sejal167kh@gmail.com>
2020-08-21 11:57:21 +0530

    initial commit

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by sejal khanna on 19/08/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
